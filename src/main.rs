0a274f30b729b561ddf74565e42df6b064acc832
at0mCrafter <kousei@riseup.net>
2021-06-18 22:09:34 +0300

    changed while loops to for loops

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ fn main() {
 fn generate_password(diceware: String) -> String {
     let args: Vec<String> = env::args().collect();
     let mut result: String = String::new();
-    let mut iter = 0;
     let word_count: i32 = if args.len() < 2 {
         3
     } else {
@@ -18,20 +17,23 @@ fn generate_password(diceware: String) -> String {
         args[2].parse::<i32>().unwrap()
     };
     let contents = fs::read_to_string(diceware).expect("Something went wrong reading the file");
-    while iter < word_count {
+    for mut i in 0..word_count {
         result += contents
             .lines()
             .nth(rand::thread_rng().gen_range(1..7776))
             .expect("line couldn't be found");
-        iter += 1;
-        if iter == 1 {
-            result = result.to_ascii_uppercase();
+        i += 1;
+        if i != 1 {
+            continue;
         }
+        result = result.to_ascii_uppercase();
     }
-    iter = 0;
-    while iter < num_count {
+    for mut i in 0..num_count {
         result += &rand::thread_rng().gen_range(1..10).to_string();
-        iter += 1;
+        i += 1;
+        if i ==  1 {
+            continue;
+        }
     }
     result
 }
