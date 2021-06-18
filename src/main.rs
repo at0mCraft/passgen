2daca639ce6cdf9af230cbe2279248fa645dc331
at0mCrafter <kousei@riseup.net>
2021-06-18 11:34:01 +0300

    added arguments and made little updates

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
 use rand::Rng;
-use std::fs;
+use std::{env, fs};
 fn main() {
     println!("{}", generate_password("diceware.txt".to_string()))
 }
 fn generate_password(diceware: String) -> String {
+    let args: Vec<String> = env::args().collect();
     let mut result: String = String::new();
     let mut iter = 0;
+    let word_count: i32 = if args.len() < 2 {
+        3
+    } else {
+        args[1].parse::<i32>().unwrap()
+    };
+    let num_count: i32 = if args.len() < 3 {
+        5
+    } else {
+        args[2].parse::<i32>().unwrap()
+    };
     let contents = fs::read_to_string(diceware).expect("Something went wrong reading the file");
-    while iter < 5 {
-        result += contents.lines().nth(rand::thread_rng().gen_range(1..7776)).expect("line couldn't be found");
+    while iter < word_count {
+        result += contents
+            .lines()
+            .nth(rand::thread_rng().gen_range(1..7776))
+            .expect("line couldn't be found");
         iter += 1;
         if iter == 1 {
             result = result.to_ascii_uppercase();
         }
     }
     iter = 0;
-    while iter < 5 {
-        result += &rand::thread_rng().gen_range(1..6).to_string();
+    while iter < num_count {
+        result += &rand::thread_rng().gen_range(1..10).to_string();
         iter += 1;
     }
     result
